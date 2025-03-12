1d5b09b5b6b1d29f810f7bb5d64c270095cdcdc4
System233 <20336040+System233@users.noreply.github.com>
2025-03-13 04:10:35 +0800

    fix: 修复某些叠加目录的权限不正确

--- build-aux/build-aux/setup-filesystem.sh
+++ build-aux/build-aux/setup-filesystem.sh
@@ -2,7 +2,7 @@
 
 test -d "linglong/filesystem/diff" && cp -arfT "linglong/filesystem/diff" "$PREFIX"
 find $PREFIX \( -type c -or -name ".wh.*" \) -exec rm -rf {} \;
-
+chmod a+Xr -R "$PREFIX"
 rm -fv "$PREFIX/etc/resolv.conf" \
     "$PREFIX/etc/localtime" \
     "$PREFIX/etc/timezone" \
