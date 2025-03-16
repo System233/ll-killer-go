c7f2f568fb9fcf95bc443ac3a4bccae73d545f1a
System233 <20336040+System233@users.noreply.github.com>
2025-03-17 06:28:12 +0800

    fix: 准备文件系统阶段过滤特殊文件(socket/fifo/block)

--- build-aux/build-aux/setup-filesystem.sh
+++ build-aux/build-aux/setup-filesystem.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 test -d "linglong/filesystem/diff" && cp -arfT "linglong/filesystem/diff" "$PREFIX"
-find $PREFIX \( -type c -or -name ".wh.*" \) -exec rm -rf {} \;
+find $PREFIX \( -type c,b,p,s -or -name ".wh.*" \) -exec rm -rf {} \;
 chmod a+Xr -R "$PREFIX"
 rm -fv "$PREFIX/etc/resolv.conf" \
     "$PREFIX/etc/localtime" \
