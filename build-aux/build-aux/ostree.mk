9e4fbe8408aea89058f0dd0ec8c39a1e6707dd3e
System233 <20336040+System233@users.noreply.github.com>
2025-03-17 06:26:39 +0800

    fix: 改进ostree仓库管理；Makefile添加ostree/ptrace功能开关

--- build-aux/build-aux/ostree.mk
+++ build-aux/build-aux/ostree.mk
@@ -1,52 +1,35 @@
-
-TARGET ?= ost.mk
--include $(TARGET)
-OSTREE_ROOT ?= $(shell realpath ~/.cache/linglong-builder/repo/)
+OSTREE_ROOT ?= $(shell realpath ~/.cache/linglong-builder/repo)
 OSTREE_NAME ?= stable
 OSTREE_REMOTE ?= https://mirror-repo-linglong.deepin.com/repos/stable
 OSTREE=ostree --repo=$(OSTREE_ROOT)
 
 ID ?= 
 MODULE ?= binary
-ifneq ($(ID),)
-	REF ?= $(ID)/$(MODULE)
-	REF_FILTER = $(shell echo "$(REF)" | sed -E -e 's\#:\#/\#' -e 's:(\/[0-9]+\.[0-9]+\.[0-9]+)/:\1.*/:')
-	REF_REMOTE_NAME ?= $(shell $(OSTREE) remote refs $(OSTREE_NAME)|grep "$(REF_FILTER)"|tail -n1)
-	REF_NAME ?= $(shell echo "$(REF_REMOTE_NAME)"|cut -d: -f2)
-	OSTREE_TARGET ?= $(OSTREE_ROOT)/layers/$(REF_NAME)
-	OSTREE_TARGET_FILES ?= $(OSTREE_TARGET)/files
-else
-	TARGET=
-endif
+REF_ID = $(shell echo "$(ID)" | sed -E -e 's@:@/@')
+REF_FILTER = $(shell echo "$(REF_ID)/($(MODULE)|runtime)" | sed -E -e 's:(\/[0-9]+\.[0-9]+\.[0-9]+)/:\1.*/:')
+REF_REMOTE_NAME = $(shell $(OSTREE) remote refs $(OSTREE_NAME)|grep -P "$(REF_FILTER)"|tail -n1)
+OSTREE_REF_DIR = $(OSTREE_ROOT)/../layers/$(REF_ID)
+OSTREE_MODULE_DIR = $(OSTREE_REF_DIR)/$(MODULE)
 
 $(OSTREE_ROOT):
 	mkdir -p $(OSTREE_ROOT)
 	$(OSTREE) init --mode=bare-user-only 
 	$(OSTREE) remote add $(OSTREE_NAME) $(OSTREE_REMOTE) --no-gpg-verify 
 
-$(OSTREE_TARGET_FILES):
-	mkdir -p $(OSTREE_TARGET)
-	rm -df $(OSTREE_TARGET)
-	$(OSTREE) checkout $(REF_REMOTE_NAME) $(OSTREE_TARGET) || true
-	test -d $(OSTREE_TARGET_FILES)
-
-$(TARGET): $(OSTREE_TARGET_FILES)
-	@echo "REF ?= $(REF)" >$@
-	@echo "REF_FILTER ?= $(REF_FILTER)" >>$@
-	@echo "REF_REMOTE_NAME ?= $(REF_REMOTE_NAME)" >>$@
-	@echo "REF_NAME ?= $(REF_NAME)" >>$@
-	@echo "OSTREE_TARGET ?= $(OSTREE_TARGET)" >>$@
-	@echo "OSTREE_ROOT ?= $(OSTREE_ROOT)" >>$@
-	@echo "OSTREE_NAME ?= $(OSTREE_NAME)" >>$@
-	@echo "OSTREE_REMOTE ?= $(OSTREE_REMOTE)" >>$@
+$(OSTREE_MODULE_DIR): $(if($(wildcard $(OSTREE_ROOT))),,$(OSTREE_ROOT))
+	mkdir -p $(OSTREE_REF_DIR)
+	$(eval REF_NAME := $(REF_REMOTE_NAME))
+	$(OSTREE) pull $(REF_NAME)
+	$(OSTREE) checkout $(REF_NAME) $(OSTREE_MODULE_DIR)
+	touch $(OSTREE_MODULE_DIR)
 
 clean:
 	rm -rf $(OSTREE_TARGET)
 
-show: 
-	@echo $(OSTREE_TARGET_FILES)
+show: $(OSTREE_MODULE_DIR)
+	@echo $(OSTREE_MODULE_DIR)/files
 
-all: $(TARGET)
+all: $(OSTREE_MODULE_DIR)
 
-.PHONY: all show
-.DEFAULT_GOAL := all 
+.PHONY: all show clean
+.DEFAULT_GOAL := all
