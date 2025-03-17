f85ec7f1391219d01cb1ae4f587d1fcd57496393
System233 <20336040+System233@users.noreply.github.com>
2025-03-17 21:55:35 +0800

    fix: ostree当id为空时，不要搜索依赖

--- build-aux/build-aux/ostree.mk
+++ build-aux/build-aux/ostree.mk
@@ -5,11 +5,21 @@ OSTREE=ostree --repo=$(OSTREE_ROOT)
 
 ID ?= 
 MODULE ?= binary
+ifneq ($(ID),)
 REF_ID = $(shell echo "$(ID)" | sed -E -e 's@:@/@')
 REF_FILTER = $(shell echo "$(REF_ID)/($(MODULE)|runtime)" | sed -E -e 's:(\/[0-9]+\.[0-9]+\.[0-9]+)/:\1.*/:')
 REF_REMOTE_NAME = $(shell $(OSTREE) remote refs $(OSTREE_NAME)|grep -P "$(REF_FILTER)"|tail -n1)
+
+ifneq ($(REF_REMOTE_NAME),)
 OSTREE_REF_DIR = $(OSTREE_ROOT)/../layers/$(REF_ID)
 OSTREE_MODULE_DIR = $(OSTREE_REF_DIR)/$(MODULE)
+OSTREE_TARGET_DIR = $(OSTREE_MODULE_DIR)/files
+else
+$(error "找不到指定的依赖:ID=$(ID) MODULE=$(MODULE)")
+endif
+
+endif
+
 
 $(OSTREE_ROOT):
 	mkdir -p $(OSTREE_ROOT)
@@ -27,7 +37,7 @@ clean:
 	rm -rf $(OSTREE_TARGET)
 
 show: $(OSTREE_MODULE_DIR)
-	@echo $(OSTREE_MODULE_DIR)/files
+	@echo $(OSTREE_TARGET_DIR)
 
 all: $(OSTREE_MODULE_DIR)
 
