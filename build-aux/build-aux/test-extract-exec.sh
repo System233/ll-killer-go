f2f3132683f70f896eca7d3c1f280f07a528f988
System233 <20336040+System233@users.noreply.github.com>
2025-03-28 15:16:38 +0800

    fix: 修复当没有任何desktop文件时，测试程序无限等待

--- build-aux/build-aux/test-extract-exec.sh
+++ build-aux/build-aux/test-extract-exec.sh
@@ -8,8 +8,8 @@ if [ ${#FILTER_LIST[@]} -eq 0 ]; then
 fi
 echo "启动项排除列表：${FILTER_LIST[@]}"
 FILTER=$(printf "%s\s*=\s*true\n" "${FILTER_LIST[@]}"|paste -sd '|')
-awk -v RS='\n\\[Desktop Entry\\]' "
+find "${SHARE_DIR}/applications" -name "*.desktop" -not -path "*/screensavers/*" -exec cat {} \; | awk -v RS='\n\\[Desktop Entry\\]' "
     /${FILTER}/ { next }
     match(\$0, /\nExec=([^\n]*)/, exec) { print exec[1] }
-" $(find "${SHARE_DIR}/applications" -name "*.desktop" -not -path "*/screensavers/*") | sed -E -e "s:['\"]?%[fFuUdDNcik]['\"]?::g" >"$OUTPUT"
+"  | sed -E -e "s:['\"]?%[fFuUdDNcik]['\"]?::g" >"$OUTPUT"
 
