655e9980e089eeeb2622016b26638bf39c9688f1
System233 <20336040+System233@users.noreply.github.com>
2025-04-23 18:30:20 +0800

    fix: 删除文件的SUID/SGID权限

--- build-aux/build-aux/setup-filesystem.sh
+++ build-aux/build-aux/setup-filesystem.sh
@@ -2,7 +2,7 @@
 
 test -d "linglong/filesystem/diff" && cp -arfT "linglong/filesystem/diff" "$PREFIX"
 find $PREFIX \( -type c,b,p,s -or -name ".wh.*" \) -exec rm -rf {} \;
-chmod a+Xr -R "$PREFIX"
+chmod a+Xr-s -R "$PREFIX"
 rm -fv "$PREFIX/etc/resolv.conf" \
     "$PREFIX/etc/localtime" \
     "$PREFIX/etc/timezone" \
